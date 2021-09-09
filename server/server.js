49b5b8c481f3beb02d283cdd3523fca5b35a5e22
Artem Zekov <azekov@humans.net>
2021-09-09 10:58:47 +0300

    feature: implement project

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 9000
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(PORT, () => console.log('Server is listening'))
+  .on('error', (err) => console.log(err))
